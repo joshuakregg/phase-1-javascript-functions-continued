fb436c96ab86a72416547aee687d6adfdbebd91f
joshau kregg <kreggjoshua@gmail.com>
2023-03-15 13:47:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+} 
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(wrap = "*") {
+    return function inFunc (param = "special") {
+        return `You are ${wrap}${param}${wrap}!`
+    }
+}
